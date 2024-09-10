263476cf5e3aa918c8c69dc4f367a46b64c1a6e7
Adarsh <adarshkumarmsay@gmail.com>
2024-09-10 23:51:17 +0530

    files added

---
+++ public/script.js
@@ -0,0 +1,51 @@
+const textArea = document.getElementById("text_to_summarize");
+const submitButton = document.getElementById("submit-button");
+const summarizedTextArea = document.getElementById("summary");
+
+submitButton.disabled = true;
+
+textArea.addEventListener("input", verifyTextLength);
+submitButton.addEventListener("click", submitData);
+
+function verifyTextLength(e) {
+  const textarea = e.target;
+
+  // Verify the TextArea value.
+  if (textarea.value.length > 200 && textarea.value.length < 100000) {
+    // Enable the button when text area has value.
+    submitButton.disabled = false;
+  } else {
+    // Disable the button when text area is empty.
+    submitButton.disabled = true;
+  }
+}
+
+function submitData(e) {
+  submitButton.classList.add("submit-button--loading");
+
+  const text_to_summarize = textArea.value;
+
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+    "text_to_summarize": text_to_summarize
+  });
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch('/summarize', requestOptions)
+    .then(response => response.text())
+    .then(summary => {
+      summarizedTextArea.value = summary;
+      submitButton.classList.remove("submit-button--loading");
+    })
+    .catch(error => {
+      console.log(error.message);
+    });
+}
